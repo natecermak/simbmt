3fceb443bf230866c827139b9e56e22cd1c833e1
natecermak <cerman07@protonmail.com>
2022-07-05 23:03:12 -0700

    get_accessible_region seems to be working. updatead tex

--- trigonometry.py
+++ trigonometry.py
@@ -16,10 +16,6 @@ def test_is_acute_triangle():
 
 
 
-if __name__ == "__main__":
-    test_is_acute_triangle()
-
-
 def get_accessible_region(p, b1, b2, t1: float, vp: float, vb: float):
     # coerce, ensure they're numpy arrays
     p = np.array(p)
@@ -38,20 +34,42 @@ def get_accessible_region(p, b1, b2, t1: float, vp: float, vb: float):
     b = 2 * (alpha @ beta / vp**2 - t1 / vb)
     c = np.linalg.norm(alpha)**2 / vp**2 - t1**2
 
-    discriminant = b**2 - 4 * a * c
-    if discriminant < 0:
-        # no roots, therefore there is no accessible region of the segment
-        return None
-    elif discriminant >= 0:
+    if a == 0: # linear or constant time difference
+        if b == 0: # constant time difference
+            return [b1, b2] if c < 0 else None
+        # linear time difference
+        root = -c / b
+        d1 = root if b < 0 else 0
+        d2 = d_max if b < 0 else root
+    else:
+        # quadratic time difference
+        discriminant = b**2 - 4 * a * c
+        if discriminant < 0:
+            # no roots, therefore there is no accessible region of the segment
+            return None
+
+        # at least one root
         roots = (-b + np.array([-1, 1]) * np.sqrt(discriminant)) / (2 * a)
         # find overlap between root interval and [0, b2-b1]
         d1 = np.max([0, roots[0]])
         d2 = np.min([d_max, roots[1]])
-        if d2 >= d1:
-            return [b1 + d1 * beta, b1 + d2 * beta]
-        else:
-            return None
+
+    if d2 >= d1:
+        return [b1 + d1 * beta, b1 + d2 * beta]
+    else:
+        return None
 
 
+def test_get_accessible_region():
+    p = (0,0)
+    b1 = (1, -1)
+    b2 = (1, 1)
 
+    print(get_accessible_region(p, b1, b2, t1=0, vp=1, vb=1))
+    print(get_accessible_region(p, b1, b2, t1=1.41, vp=1, vb=1))
+    print(get_accessible_region(p, b1, b2, t1=1, vp=1, vb=10))
+
+if __name__ == "__main__":
+    test_is_acute_triangle()
+    test_get_accessible_region()
 
